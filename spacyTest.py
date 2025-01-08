d21b958862080b4a7e3d7aa9eb8f59cace7f6c5c
Zhouyi-Yu <zhouyi1@ualberta.ca>
2025-01-07 18:31:59 -0700

    2nd

--- spacyTest.py
+++ spacyTest.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+with open("data/wiki_us.txt", "r") as f:
+    text = f.read()
+
+doc = nlp(text)
+
+# print(doc)
+# print(len(doc))
+# print(len(text))
+
+words = text.split()[:10]
+
+i=5
+for token in doc[i:8]:
+    print (f"SpaCy Token {i}:\n{token}\nWord Split {i}:\n{words[i]}\n\n")
+    i=i+1
+    
